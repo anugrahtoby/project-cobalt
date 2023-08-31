3d1c3ba3053bd8305d413335baa5abeac230c0f9
Anugrah <anugrah.toby@gmail.com>
2023-08-31 12:31:10 +0530

    updated primary-button component and added a dshboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import PrimaryButton from '../components/atomic/primary-button/PrimaryButton'
 
 export default function Home() {
   return (
     <div>
-      <h1>Cobalt.</h1>
+      <div>
+        <h1>Cobalt.</h1>
+      </div>
+      <PrimaryButton />
     </div>
   )
 }
