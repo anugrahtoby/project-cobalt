07ec958c9da0bb5e7ad760a5b6dd84576b3cea13
aswinskumar264 <aswinskumar264@gmail.com>
2023-11-12 00:30:05 +0530

    Reg New

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Login from "./Login"
+const page = () => {
+  return <div><Login /></div>;
+};
+
+export default page;
---
+++ app/registration/Registration.jsx
@@ -0,0 +1,35 @@
+/* Code generated with AutoHTML Plugin for Figma */
+import "./Registration.scss";
+
+export const Register = ({ ...props }) => {
+  return (
+    <div className="register">
+    <div><img src="./aayush.png"/></div>
+      <div className="have-an-account-already-login">
+        <span>
+          <span className="have-an-account-already-login-span">
+            Have an account already?
+          </span>
+          <span className="have-an-account-already-login-span2"> Login</span>
+        </span>{" "}
+      </div>
+      <div className="line-2"></div>
+      <div className="dk-text-box"></div>
+      <div className="dk-text-box2"></div>
+      <div className="dk-text-box3"></div>
+      <div className="name">Name </div>
+      <div className="password">Password </div>
+      <div className="confirm-password">Confirm Password </div>
+      <div className="dk-text-box4"></div>
+      <div className="dk-text-box5"></div>
+      <div className="e-mail-id">E-mail ID </div>
+      <div className="gender">Gender </div>
+      <div className="cobalt">Cobalt </div>
+      <button className="primary-button">
+        <div className="create-account">Create Account </div>
+      </button>
+    </div>
+  );
+};
+
+export default Register;
